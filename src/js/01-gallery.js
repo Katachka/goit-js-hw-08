5b13003c740ea002e9cae968f1bcc78f7494b2f2
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-02-17 19:04:54 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', cardsMarkup);
+
+
+function createImageCardsMarkup(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+  </li>`;
+    })
+        .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt",
+    captionPosition: 'bottom',
+});
+console.log(createImageCardsMarkup(galleryItems));
+console.log(galleryContainer);
 console.log(galleryItems);
